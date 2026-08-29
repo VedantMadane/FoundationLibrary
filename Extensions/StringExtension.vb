3926ff68e3af79a97817f97eb3ea9a9b89fe402e
kourt3 <kourt35@gmail.com>
2026-08-30 00:02:28 +0300

    Add HTML Library

--- Extensions/StringExtension.vb
+++ Extensions/StringExtension.vb
@@ -7,7 +7,7 @@ Namespace Extensions
 
 #Region "Letters Methods"
         <Extension>
-        Public Function IsHasUpperLetter(Str As String, Optional NumberCountUpperLetter As Integer? = Nothing) As Boolean
+        Function IsHasUpperLetter(Str As String, Optional NumberCountUpperLetter As Integer? = Nothing) As Boolean
 
             Dim CountUppers As Integer = 0
             For i = 0 To Str.Length - 1
@@ -20,7 +20,7 @@ Namespace Extensions
         End Function
 
         <Extension>
-        Public Function IsHasLowerLetter(Str As String, Optional NumberCountLowerLetter As Integer? = Nothing) As Boolean
+        Function IsHasLowerLetter(Str As String, Optional NumberCountLowerLetter As Integer? = Nothing) As Boolean
             Dim CountLower As Integer = 0
             For i = 0 To Str.Length - 1
                 If Char.IsLetter(Str(i)) And Char.ToUpper(Str(i)) = Str(i) Then CountLower += 1
@@ -31,7 +31,7 @@ Namespace Extensions
         End Function
 
         <Extension>
-        Public Function ContainsOnlyLettersWithException(Str As String, ParamArray Exceptions() As String) As Boolean
+        Function ContainsOnlyLettersWithException(Str As String, ParamArray Exceptions() As String) As Boolean
             For i = 0 To Str.Length - 1
                 If Char.IsLetter(Str(i)) AndAlso Exceptions.Contains(Str(i)) = True Then Return False
                 If Not Char.IsLetter(Str(i)) Then Return False
@@ -40,7 +40,7 @@ Namespace Extensions
         End Function
 
         <Extension>
-        Public Function ContainsOnlyLetters(Str As String) As Boolean
+        Function ContainsOnlyLetters(Str As String) As Boolean
             For i = 0 To Str.Length - 1
                 If Not Char.IsLetter(Str(i)) Then Return False
             Next
@@ -48,7 +48,7 @@ Namespace Extensions
         End Function
 
         <Extension>
-        Public Function AreAllUpperCase(Str As String) As Boolean
+        Function AreAllUpperCase(Str As String) As Boolean
             For i = 0 To Str.Length - 1
                 If Char.IsLower(Str(i)) Then Return False
             Next
@@ -56,7 +56,7 @@ Namespace Extensions
         End Function
 
         <Extension>
-        Public Function AreAllLowerCase(Str As String) As Boolean
+        Function AreAllLowerCase(Str As String) As Boolean
             For i = 0 To Str.Length - 1
                 If Char.IsUpper(Str(i)) Then Return False
             Next
@@ -66,7 +66,7 @@ Namespace Extensions
 
 #Region "Digit Methods"
         <Extension>
-        Public Function IsHasDigit(Str As String, Optional NumberCountDigit As Integer? = Nothing) As Boolean
+        Function IsHasDigit(Str As String, Optional NumberCountDigit As Integer? = Nothing) As Boolean
             Dim Count As Integer = 0
             For i = 0 To Str.Length - 1
                 If Char.IsDigit(Str(i)) Then Count += 1
@@ -75,7 +75,7 @@ Namespace Extensions
             Return False
         End Function
         <Extension>
-        Public Function ContainsOnlyDigitsWithExceptions(Str As String, ParamArray Exceptions() As Integer) As Boolean
+        Function ContainsOnlyDigitsWithExceptions(Str As String, ParamArray Exceptions() As Integer) As Boolean
             For i = 0 To Str.Length - 1
                 If Char.IsDigit(Str(i)) = True AndAlso Exceptions.Contains(AscW(Str(i))) = True Then Return False
                 If Not Char.IsDigit(Str(i)) Then Return False
@@ -84,7 +84,7 @@ Namespace Extensions
         End Function
 
         <Extension>
-        Public Function ContainsOnlyDigits(str As String) As Boolean
+        Function ContainsOnlyDigits(str As String) As Boolean
             For i = 0 To str.Length - 1
                 If Not Char.IsDigit(str(i)) Then Return False
             Next
@@ -93,18 +93,18 @@ Namespace Extensions
 #End Region
 
         <Extension>
-        Public Function IsEmail(Str As String) As Boolean
+        Function IsEmail(Str As String) As Boolean
             If Str.Contains("@") Then Return True
             Return False
         End Function
         <Extension>
-        Public Function IsWebSite(Str As String) As Boolean
+        Function IsWebSite(Str As String) As Boolean
             If Str.ToUpper.Contains("HTTP:\\") Or Str.ToUpper.Contains("HTTPS:\\") Or Str.ToUpper.Contains("WWW.") Or Str.ToUpper.Contains(".COM") Then Return True
             Return False
         End Function
 
         <Extension>
-        Public Function ContainsSpecialCharacter(Str As String) As Boolean
+        Function ContainsSpecialCharacter(Str As String) As Boolean
             If String.IsNullOrEmpty(Str) Then Return True
             For Each c As Char In Str
                 If Not Char.IsLetterOrDigit(c) AndAlso Char.IsWhiteSpace(c) Or Char.IsSymbol(c) Or Char.IsSeparator(c) Then Return True
@@ -112,7 +112,7 @@ Namespace Extensions
             Return False
         End Function
         <Extension>
-        Public Function ContainsSymbols(Str As String) As Boolean
+        Function ContainsSymbols(Str As String) As Boolean
             For i = 0 To Str.Length - 1
                 If Char.IsSymbol(Str(i)) Then Return True
             Next
@@ -120,20 +120,56 @@ Namespace Extensions
         End Function
 
         <Extension>
-        Public Function ContainsAny(Str As String, ParamArray ListStrings() As String) As Boolean
+        Function ReplaceChars(Str As String, Chr As Char(), ReplaceString As String) As String
+            Dim ReturnStr As String = Nothing
+            For i = 0 To Str.Length - 1
+                If Chr.Contains(Str(i)) Then
+                    ReturnStr += ReplaceString
+                    Continue For
+                End If
+                ReturnStr += Str(i)
+            Next
+            Return ReturnStr
+        End Function
+        <Extension>
+        Function PadAt(Str As String, Index As Integer, Times As Integer, Chr As Char) As String
+
+            For i = 0 To Times - 1
+                If Str = Nothing Then
+                    Str = Chr
+                    Continue For
+                End If
+                Str = Str.Insert(Index, Chr)
+            Next
+            Return Str
+        End Function
+
+
+        <Extension>
+        Function HowManyTimesExistChar(Str As String, Chr As Char) As Integer
+            Dim Count As Integer = 0
+            For i = 0 To Str.Length - 1
+                If Str(i) = Chr Then
+                    Count += 1
+                End If
+            Next
+            Return Count
+        End Function
+        <Extension>
+        Function ContainsAny(Str As String, ParamArray ListStrings() As String) As Boolean
             For i = 0 To ListStrings.Length - 1
                 If Str.Contains(ListStrings(i)) Then Return True
             Next
             Return False
         End Function
         <Extension>
-        Public Function IsLengthInRange(Str As String, MinLength As Integer?, MaxLength As Integer?) As Boolean
+        Function IsLengthInRange(Str As String, MinLength As Integer?, MaxLength As Integer?) As Boolean
             If MinLength IsNot Nothing AndAlso Str.Length < MinLength Then Return False
             If MaxLength IsNot Nothing AndAlso Str.Length > MaxLength Then Return False
             Return True
         End Function
         <Extension>
-        Public Function TryConvertToInt(Str As String, Optional ByRef Result As Integer = Nothing) As Boolean
+        Function TryConvertToInt(Str As String, Optional ByRef Result As Integer = Nothing) As Boolean
             If Integer.TryParse(Str, Result) Then Return True
             Return False
         End Function
