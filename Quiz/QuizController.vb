0012b7c1d2ce9c96537d703dcd9721d32d91ec96
kourt3 <kourt35@gmail.com>
2026-08-30 22:21:08 +0300

    i fixed More

---
+++ Quiz/QuizController.vb
@@ -0,0 +1,73 @@
+﻿Imports FoundationLibrary.Quiz.Data
+Namespace Quiz
+
+
+    Public Class QuizController
+
+        Public Shared Index As Integer
+
+        Shared Function Random_Quest(ByVal Questions() As QuizData) As QuizData()
+            Dim Index As Integer = Nothing
+            Dim Erwthseis(Questions.Count - 1) As QuizData
+            Dim ListIndexs As New List(Of Integer)
+            Randomize()
+
+            While ListIndexs.Count - 1 <> Questions.Count - 1
+                Do
+                    Index = Int((Questions.Count) * Rnd())
+                Loop Until ListIndexs.Contains(Index) = False
+                ListIndexs.Add(Index)
+
+            End While
+
+            For i = 0 To ListIndexs.Count - 1
+                Erwthseis(i) = Questions(ListIndexs(i))
+            Next
+
+            Return Erwthseis
+        End Function
+
+
+
+
+        Shared Function Random_Answers(ByVal ListAnswers As List(Of Answers)) As List(Of Answers)
+            Dim index As Integer
+            Dim ListIndexs As New List(Of Integer)
+            Randomize()
+            While ListIndexs.Count - 1 <> ListAnswers.Count - 1
+                Do
+                    index = Int((ListAnswers.Count) * Rnd())
+                Loop Until ListIndexs.Contains(index) = False
+                ListIndexs.Add(index)
+            End While
+
+            Dim ReturnListAnswer As New List(Of Answers)
+            For i = 0 To ListIndexs.Count - 1
+                ReturnListAnswer.Add(ListAnswers(ListIndexs(i)))
+            Next
+
+
+            Return ReturnListAnswer
+        End Function
+
+        Shared Function Next_Question(ByVal Question() As QuizData) As QuizData
+            Index += 1
+            If Question.Count >= Index Then
+                Return Question(Index)
+            Else
+                Index = 0
+                Return Question(Index)
+            End If
+        End Function
+
+        Shared Function Preview_Question(ByVal Question() As QuizData) As QuizData
+            Index -= 1
+            If Question.Count <= Index Then
+                Return Question(Index)
+            Else
+                Index = Question.Count - 1
+                Return Question(Index)
+            End If
+        End Function
+    End Class
+End Namespace
---
+++ Quiz/QuizData.vb
@@ -0,0 +1,25 @@
+﻿Namespace Quiz.Data
+    Public Class QuizData
+        Public Id As Integer
+        Public NumQuestion As Integer
+        Public Quest As String
+        Public BookTitle As String
+        Public Page As Integer
+        Public Category As Integer
+        Public Photo As String
+        Public Sound As String
+        Public Lang As String
+        Public Answers As New List(Of Answers)
+        Public Sub Options(Num As Integer, Str As String, Optional Correct As Boolean = False, Optional Dspn As String = Nothing)
+            Answers.Add(New Answers With {.NumAnswer = Num, .Answer = Str, .Correct = Correct})
+        End Sub
+    End Class
+    Public Class Answers
+        Public Id As Integer
+        Public NumAnswer As Integer
+        Public Answer As String
+        Public Sound As String
+        Public Correct As Boolean = False
+    End Class
+End Namespace
+
